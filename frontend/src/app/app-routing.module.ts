2f9c1322b5ff83fb54ed4383f1e45713d93f9ec0
Jakub Wincenciak <47039532+enshin258@users.noreply.github.com>
2020-11-16 18:38:14 +0100

    Router created (#19)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './wineworld/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ProductComponent]
--- frontend/src/app/wineworld/product/product.component.ts
+++ frontend/src/app/wineworld/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsServiceService } from 'src/app/services/products-service.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productsService: ProductsServiceService){}
+
 
   ngOnInit(): void {
   }
+
+  
+  saveProduct(){
+    this.productsService.saveProduct().subscribe(()=>{});
+}
 
 }
