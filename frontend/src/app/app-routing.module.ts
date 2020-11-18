1b8fabec9e883b08690871bab631fe8eaacd64ad
enshin258 <kuba.win@wp.pl>
2020-11-18 19:45:58 +0100

    #added basic frontend components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GridViewComponent } from './grid-view/grid-view.component';
 import { ProductComponent } from './wineworld/product/product.component';
 
 const routes: Routes = [
+  {path: '', component: GridViewComponent},
   {path: 'products', component: ProductComponent}
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { ProductComponent } from './wineworld/product/product.component';
 import { LocationComponent } from './wineworld/location/location.component';
 import { ProductsServiceService } from './services/products-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GridViewComponent } from './grid-view/grid-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WineworldComponent,
     ProductComponent,
-    LocationComponent
+    LocationComponent,
+    HeaderComponent,
+    SidebarComponent,
+    GridViewComponent
   ],
   imports: [
     BrowserModule,
