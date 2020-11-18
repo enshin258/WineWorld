8efdfcbe6c9ae19f6e69d5bbec45b36b28da4342
lgorczynski98 <46427659+lgorczynski98@users.noreply.github.com>
2020-11-18 20:07:48 +0100

    Created angular components (#25)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,22 +6,30 @@ import { AppComponent } from './app.component';
 import { WineworldComponent } from './wineworld/wineworld.component';
 import { ProductComponent } from './wineworld/product/product.component';
 import { LocationComponent } from './wineworld/location/location.component';
-import { ProductsServiceService } from './services/products-service.service';
+import { ProductsService } from './services/products.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AssortmentComponent } from './wineworld/assortment/assortment.component';
+import { LoginComponent } from './wineworld/login/login.component';
+import { RegistrationComponent } from './wineworld/registration/registration.component';
+import { ProfileComponent } from './wineworld/profile/profile.component';
+import { OrderComponent } from './wineworld/order/order.component';
+import { UsersService } from './services/users.service';
+import { LocationsService } from './services/locations.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     WineworldComponent,
     ProductComponent,
-    LocationComponent
+    LocationComponent,
+    AssortmentComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ProfileComponent,
+    OrderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [ProductsServiceService],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ProductsService, UsersService, LocationsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/wineworld/assortment/assortment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assortment',
+  templateUrl: './assortment.component.html',
+  styleUrls: ['./assortment.component.css']
+})
+export class AssortmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
