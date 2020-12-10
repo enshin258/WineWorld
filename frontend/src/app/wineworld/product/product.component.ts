288e2cc2361459f83ff8a4659da6a67a360f4c82
enshin258 <kuba.win@wp.pl>
2020-12-10 21:30:28 +0100

    added product page and map

--- frontend/src/app/wineworld/product/product.component.ts
+++ frontend/src/app/wineworld/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-product',
@@ -8,8 +9,21 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ProductComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
+  map;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.map = L.map("map").setView([66, 33], 5);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    var marker = L.marker([66, 33]).addTo(this.map);
+    marker.bindPopup("<b>Winnica super winko</b><br>Najlepsze winko u nas").openPopup();
+
+
+  }
 
   saveProduct() {
     // this.productsService.saveProduct().subscribe(() => {});
