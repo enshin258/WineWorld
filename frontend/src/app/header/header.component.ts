e3f6442ce5bb807406f9ea8c4a6adff664453cab
enshin258 <kuba.win@wp.pl>
2020-12-11 17:54:54 +0100

    added login dialog

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
+    // Get the button that opens the modal
+    var btn = document.getElementById('loginBtn');
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+      modal.style.display = 'block';
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    };
+  }
 }
