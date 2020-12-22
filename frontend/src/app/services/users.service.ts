e19304561ae2c2a10e64db416877104fb46e2c40
lgorczynski98 <lgorczynski98@gmail.com>
2020-12-22 17:01:31 +0100

    Added login and registration, but login fails

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -7,17 +7,24 @@ import { User } from '../models/user';
 })
 export class UsersService {
   private addUserUrl = 'http://localhost:8080/user/add';
-  private loginUserUrl = '';
-  private changeUserInfoUrl = '';
+  private loginUserUrl = 'http://localhost:8080/login';
+  private changeUserInfoUrl = 'http://localhost:8080/user/add';
 
   constructor(private http: HttpClient) {}
 
   register(user: User) {
-    return this.http.post(this.addUserUrl, user);
+    return this.http.post(this.addUserUrl, {
+      email: user.email,
+      login: user.username,
+      password: user.password,
+    });
   }
 
   login(user: User) {
-    return this.http.post(this.loginUserUrl, user);
+    return this.http.post(this.loginUserUrl, {
+      username: user.username,
+      password: user.password,
+    });
   }
 
   changeUserInfo(user: User) {
