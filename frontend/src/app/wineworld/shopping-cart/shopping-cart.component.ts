1831395d88e62c0bce3c8d0d74b395b98260a19b
lgorczynski98 <lgorczynski98@gmail.com>
2021-01-02 19:28:40 +0100

    Added paypal integration (no work)

--- frontend/src/app/wineworld/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/wineworld/shopping-cart/shopping-cart.component.ts
@@ -1,17 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { ShoppingCartPosition } from 'src/app/models/shoping_cart_position';
 import { OrderService } from 'src/app/services/order.service';
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, AfterViewChecked {
   cartPositions: ShoppingCartPosition[];
   totalPrice: number;
 
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AdOhfeWD3g5jL89Fg-fUSkOCNR5ZlJwLbRBers55Yx3vUKbj2LdR1S-3vxemg-w2pgbJvb-Q7fPV2USK',
+      production: 'production'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transaction: [
+            {amount: {total: this.totalPrice, currency: 'USD'}}
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //on payment success
+        console.log('Pay');
+      })
+    }
+  };
+
   constructor(private orderService: OrderService) {}
+
+  ngAfterViewChecked(): void{
+    if(!this.addScript){
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+
+  addPaypalScript(){
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scriptTagElement = document.createElement('script');
+      scriptTagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scriptTagElement.onload = resolve;
+      document.body.appendChild(scriptTagElement);
+    })
+  }
 
   ngOnInit(): void {
     console.log(this.orderService.getCart());
